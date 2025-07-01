b9d5ecfac4e857c2376ef858d6623355e9b4946d
Uljr <31762855+Uljr@users.noreply.github.com>
2025-07-01 19:56:02 -0400

    Update server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const axios = require('axios');
 const cors = require('cors');
 const app = express();
+const port = process.env.PORT || 4000 
 
 app.use(cors());
 
